3a3f314fdad795bfaed7c2ed091f341f5857b265
JMD SAMEER <jmdsameer1@gmail.com>
2026-09-22 11:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (28.77%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,22 @@
+class Solution(object):
+
+  def myPow(self, x, n):
+    """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+    if n < 0:
+      x = 1 / x
+      n = -n
+
+    res = 1.0
+    current_product = x
+
+    while n > 0:
+      if n % 2 == 1:
+        res *= current_product
+      current_product *= current_product
+      n //= 2
+
+    return res
